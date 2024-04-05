6b0f799b554a6dff7e08d4a2420a0795ca1aaa08
unknown <i.paxa3000@yandex.ru>
2024-04-05 23:19:03 +0500

    django start setap

---
+++ banerApiApp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Feature(models.Model):
+    feature_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Banner(models.Model):
+    banner_id = models.AutoField(primary_key=True)
+    tag_ids = models.ManyToManyField(Tag)
+    feature_id = models.ForeignKey(Feature, on_delete=models.CASCADE)
+    content = models.JSONField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
